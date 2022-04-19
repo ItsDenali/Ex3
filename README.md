1024f23bb85484ebb76b1b8bd251331430929b8d
Nate <DeepBlueFjords@Gmail.com>
2022-04-19 16:15:18 -0400

    Created quote app
    Created quote app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Ex3
 
+JS Application that randomly generates one of three quotes in the output.
+
 "The Industrial Revolution and its consequences have been a disaster for the human race." - Theodore J Kaczynski
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 let quotes = [
+    `"The Industrial Revolution and its consequences have been a disaster for the human race."\\n- Theodore J Kaczynski`, //Zeroth element
+    `"At the end of it all, in those darkest of day, smiles man atop the mountain debris.\nHe can see the many stars, smell the coming wind, only now does he know that he's free."\n- Mike Ma`, //First element
+    `"Life is at most basic, struggle for ownership of space."\n- B.A.P.`
+    ]
 
-    `"The Industrial Revolution and its consequences have been a disaster for the human race." - Theodore J Kaczynski`, //Zeroth element
-    `"At the end of it all, in those darkest of day, smiles man atop the mountain debris. He can see the many stars, smell the coming wind, only now does he know that he's free." - Mike Ma`, //First element
-    `"Life is at most basic, struggle for ownership of space." - BAP` //Second element
-
-]
-
-//Chose between the three randomly
-console.log(quotes[Math.floor(Math.random() * 3)]) //Outputs 0 - 2.9 repeating, Math.floor drops the decimal
+var index = Math.floor((quotes.length * Math.random()))
+console.log(quotes[index])
